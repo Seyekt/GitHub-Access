23d79cfc63bd7f0bc987bc118c47bd208febb114
Thomas Keyes <keyest@tcd.ie>
2020-12-18 13:44:56 +0000

    Added repoToStr

--- githubAPI.py
+++ githubAPI.py
@@ -7,15 +7,22 @@ def getUserData(username):
 	userData = requests.get(url).json()
 	return userData
 
+def repoToStr(repo):
+	output = ""
+	output += ("Repository Name: " + repo.full_name + "\n")
+	return output
+
 def getUserRepos(username):
 	g = Github()
 	user = g.get_user(username)	
 	output = ""
 	for repo in user.get_repos():
-		output += (str(repo) + "\n")
+		#output += (str(repo) + "\n")
+		output += getRepoToStr(repo)
 	return output
+
 
 username = input("Enter GitHub username: ")
 
-pprint(getUserData(username)) 
+#pprint(getUserData(username)) 
 print(getUserRepos(username))
